5171776adbdc1e28791df51f713319b335aa0c3a
Cadel Watson <cadel@cadelwatson.com>
2016-07-30 11:11:59 +1000

    Add rule formatting to filter

--- filter.py
+++ filter.py
@@ -176,11 +176,23 @@ def create_example(example_list):
     return RawBlock('html', html)
     #return RawBlock('html', shrink_list(example_list))
 
+def create_rule(rule_list):
+    raw = shrink_list(rule_list)[3:].split(':')
+    rule_name = '<div class="rule-name">{0}</div>'.format(raw[0])
+    rule_definition = '<div class="rule-definition">{0}</div>'.format(raw[1].strip())
+    html = '<div class="rule">' + rule_name + rule_definition + '</div>'
+
+    return RawBlock('html', html)
 
 def test(key, value, format, meta):
     if key == 'OrderedList':
         if value[0][1]['t'] == 'Example':
             return create_example(value[1][0][0]['c'])
+    elif key == 'Para':
+        if value[0]['t'] == 'Str':
+            if value[0]['c'][0:3] == '(*)':
+                return create_rule(value)
+
     #return RawBlock('html', str(key) + ' ' + str(value))
 
 if __name__ == "__main__":
