4e711a2895bc8fe898b28693e07a7f6f1422e801
Cadel Watson <cadel@cadelwatson.com>
2016-08-12 19:24:00 +1000

    Recode layout using Flexbox, thanks to https://stackoverflow.com/questions/33514047/flexbox-holy-grail-layout-fixed-header-fixed-left-nav-fluid-content-area-fix

--- src/js/main.js
+++ src/js/main.js
@@ -1,18 +1,34 @@
 $(document).ready(function(){
     toggle_sidebar();
+    toggle_aside();
 });
 
 function toggle_sidebar() {
-    var sidebar_visible = true;
+    var sidebar_visible = false;
 
-    $("#toggle").click(function(){
-        console.log("something");
+    $("#sidebar-toggle").click(function(){
         if (sidebar_visible === true) {
             sidebar_visible = false;
-            $("#sidebar").animate({width: "0px", opacity: 0}, "fast");
+            $("nav").removeClass("open");
+
         } else {
             sidebar_visible = true;
-            $("#sidebar").animate({width: "200px", opacity:1}, "fast");
+            $("nav").addClass("open");
+        }
+    });
+}
+
+function toggle_aside() {
+    var aside_visible = false;
+
+    $("#aside-toggle").click(function(){
+        if (aside_visible === true) {
+            aside_visible = false;
+            $("aside").removeClass("open");
+
+        } else {
+            aside_visible = true;
+            $("aside").addClass("open");
         }
     });
 }
