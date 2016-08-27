5ee772b6748cf63222651c0adecb07ecb1672265
Cadel Watson <cadel@cadelwatson.com>
2016-08-27 17:58:39 +1000

    Remove dead css

--- src/js/main.js
+++ src/js/main.js
@@ -1,7 +1,6 @@
 $(document).ready(function(){
-    toggle_sidebar();
-
     $("div.content").sidenotes();
+
     if ($(window).width() <= 979) {
         $("div.content").sidenotes("hide");
     }
@@ -15,35 +14,3 @@ $(document).ready(function(){
     });
 
 });
-
-function toggle_sidebar() {
-    var sidebar_visible = false;
-
-    if ($(window).width() > 800) {
-        sidebar_visible = true;
-        $("nav").addClass("open");
-    }
-
-    $("#sidebar-toggle").click(function(){
-        if (sidebar_visible === true) {
-            sidebar_visible = false;
-            $("nav").removeClass("open");
-
-        } else {
-            sidebar_visible = true;
-            $("nav").addClass("open");
-        }
-    });
-
-    $(window).resize(function() {
-        if ($(this).width() <= 1000 && sidebar_visible === true) {
-            sidebar_visible = false;
-            $("nav").removeClass("open");
-        } else if ($(this).width() > 1000 && sidebar_visible === false) {
-            sidebar_visible = true;
-            $("nav").addClass("open");
-        }
-    });
-}
-
-
